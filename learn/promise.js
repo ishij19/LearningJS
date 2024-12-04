49df27c27e51ecf2e2a57c8d70cdff358e09166c
ishij19 <142041733+ishij19@users.noreply.github.com>
2024-12-04 20:24:03 +0000

    Done for 4th dec

--- learn/promise.js
+++ learn/promise.js
@@ -1,1 +1,62 @@
-console.log("ISHI");
+const promiseOne = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log('Async task is complete');
+        resolve()
+        
+    },1000)
+});
+
+promiseOne.then(function(){
+    console.log('Promise consumed');
+    
+})
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+        console.log('Async task 2');
+        resolve()
+    },1000)
+}).then(function(){
+    console.log('Async task 2 resolved');
+    
+})
+
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:"Chai", email:"chai@example.com"})
+    })
+},1000);
+
+promiseThree.then(function(user){
+    console.log(user);
+    
+})
+
+
+ const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true 
+        if(!error){
+            resolve({username:"Ishui", password:"12ef"})
+        }
+        else{
+            reject('ERROR:Something went wrong')
+        }
+    },1000)
+ });
+
+ promiseFour.then((user)=>{
+    console.log(user);
+    return user.username
+ }).then((username)=>{
+    console.log(username);
+    
+ }).catch(function(error){
+    console.log(error);
+    
+ }).finally(()=>console.log("Either resolved or rejected")
+ )
+
+
+ 
